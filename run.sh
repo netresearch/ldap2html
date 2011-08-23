740153ca9d11176bc57c9706d6048a9766660c34
Christian Weiske <christian.weiske@netresearch.de>
2011-08-23 13:17:26 +0200

    NRFE-183: run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ ! -d entries ]; then
+    mkdir entries
+else
+    rm entries/*
+fi
+php fetch-from-ldap.php --quiet
+
+if [ ! -d html ]; then
+    mkdir html
+else
+    rm html/*
+fi
+php gen-html.php
